a4fe24dc1e22ca0698e0cd1a6ee3379f1e0e81a9
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-02-08 21:30:57 -0400

    added morgan

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 const { login, getKeys, revokeKeys, createKey, getCookie, getIP } = require('./utils')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(morgan('dev'))
 
 app.post('/', async (req, res) => {
   try {
